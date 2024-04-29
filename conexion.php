da3e88aa91d04fd932944ca3f457766b120ee8cf
ferayala-csm <131723368+ferayala-csm@users.noreply.github.com>
2024-04-28 22:55:05 -0300

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -7,9 +7,10 @@ $database = "prime";
 // Crea la conexión
 $conn = new mysqli($servername, $username, $password, $database);
 
-// Verifica la conexión
+// Verifica la conexión y muestra un mensaje
 if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
+    echo "Conexión fallida: " . $conn->connect_error;
+} else {
+    echo "Conexión exitosa";
 }
-echo "Conexión exitosa";
 ?>
